0cef9fbc95f4200f483b79cd81979d1051add992
ChiChiBeek <me@thijs.com>
2025-11-14 15:01:50 +0100

    Update redirect

--- src/msalConfig.js
+++ src/msalConfig.js
@@ -5,7 +5,7 @@ export const msalConfig = {
   auth: {
     clientId: "a71da847-7471-4d52-8bdd-de69f22d1c9e",
     authority: "https://login.microsoftonline.com/c0a7ac20-aa6a-43ff-b346-a5b54575705c",
-    redirectUri: "http://localhost:3000",
+    redirectUri: window.location.origin,
   },
   cache: {
     cacheLocation: "localStorage",
