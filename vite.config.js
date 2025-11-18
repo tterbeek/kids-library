4e59eccea1040241e732540441f40b8498eee35f
ChiChiBeek <me@thijs.com>
2025-11-18 10:42:32 +0100

    Initial Vite version

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,9 @@ export default defineConfig({
       registerType: "autoUpdate",
       injectRegister: "auto", // automatically registers SW in prod
       includeAssets: ["icons/192.png", "icons/512.png", "icons/1024.png"],
-      manifest: false, // we use public/manifest.json instead
+      manifest: true, // we use public/manifest.json instead
       workbox: {
-        globPatterns: ["**/*.{js,css,html,png,svg,jpg,jpeg}"],
+        globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
         navigateFallback: "/index.html",
         runtimeCaching: [
           {
